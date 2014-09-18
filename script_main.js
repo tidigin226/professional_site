c330139a1434ee4030929924c8aa931d777d9d6c
Tim Hyon <tim.c.hyon@gmail.com>
2014-09-18 03:43:22 -0700

    new site design

--- script_main.js
+++ script_main.js
@@ -1,70 +1,90 @@
 $(document).ready(function() {
+      $("#bluebar").css("display","none");
   $("#projects").hide();
   $("#about").hide();
   $("#resume").hide();
   $("#projectsLink").click(function() {
     if($(this).css("background")==colStrings["chrome_Red"]){
       $("#projects").hide(300);
-      $(this).css("background","#FFF");
-      $(this).css("color","#FFF");
-      $("body").css("background","#FFF");
+      $(this).css("background","transparent");
+      $(this).css("color","#000");
+      /*$("body").css("background","#FFF");*/
+      /*$("#bluebar").css("background","#000");*/
+      $("#bluebar").css("display","none");
       $("#topBuffer").animate({height:200},300); 
+      $(".container").css("background","transparent");        
     }
     else{
+        $(".container").css("background","rgba(200,200,200,0.5)");        
+        $("#bluebar").css("display","inline");
         $("#resume").hide(300);
         $("#about").hide(300);
         $("#projects").show(300);
-        $("#resumeLink").css("background","#FFF");
+        $("#resumeLink").css("background","transparent");
         $("#resumeLink").css("color","#000");
-        $("#aboutLink").css("background","#FFF");
+        $("#aboutLink").css("background","transparent");
         $("#aboutLink").css("color","#000");
         $(this).css("background","#C63D0F");
+        $("#bluebar").css("background","#C63D0F");
         $(this).css("color","#FFF");
-        $("body").css("background","#FFF");
+        /*$("body").css("background","#FFF");*/
         $("#topBuffer").animate({height:30},300);
     }
   });
   $("#aboutLink").click(function() {
     if($(this).css("background")==colStrings["chrome_Green"]){
       $("#about").hide(300);
-      $(this).css("background","#FFF");
+      $(this).css("background","transparent");
       $(this).css("color","#000");
-      $("body").css("background","#FFF");
+      /*$("body").css("background","#FFF");*/
+      /*$("#bluebar").css("background","#000");*/
+      $("#bluebar").css("display","none");
       $("#topBuffer").animate({height:200},300); 
+      $(".container").css("background","transparent");        
     }
     else{
+        $(".container").css("background","rgba(200,200,200,0.5)");        
+        $(".container").css("height","100%;");        
+        $("#bluebar").css("display","inline");
         $("#projects").hide(300);
         $("#resume").hide(300);
         $("#about").show(300);
-        $("#resumeLink").css("background","#FFF");
+        $("#resumeLink").css("background","transparent");
         $("#resumeLink").css("color","#000");
-        $("#projectsLink").css("background","#FFF");
+        $("#projectsLink").css("background","transparent");
         $("#projectsLink").css("color","#000");
         $(this).css("background","#7E8F7C");
+        $("#bluebar").css("background","#7E8F7C");
         $(this).css("color","#FFF");
-        $("body").css("background","#FFF");
+        /*$("body").css("background","#FFF");*/
         $("#topBuffer").animate({height:30},300);
     }
   });
   $("#resumeLink").click(function() {
     if($(this).css("background")==colStrings["chrome_Blue"]){
       $("#resume").hide(300);
-      $(this).css("background","#FFF");
+      $(this).css("background","transparent");
       $(this).css("color","#000");
-      $("body").css("background","#FFF");
+      /*$("body").css("background","#FFF");*/
+      /*$("#bluebar").css("background","#000");*/
+      $("#bluebar").css("display","none");
       $("#topBuffer").animate({height:200},300); 
+      $(".container").css("background","transparent");        
     }
     else{
+        $(".container").css("background","rgba(200,200,200,0.5)");        
+        $("#bluebar").css("display","inline");
         $("#projects").hide(300);
         $("#about").hide(300);
         $("#resume").show(300);
-        $("#projectsLink").css("background","#FFF");
+        $("#projectsLink").css("background","transparent");
         $("#projectsLink").css("color","#000");
-        $("#aboutLink").css("background","#FFF");
+        $("#aboutLink").css("background","transparent");
         $("#aboutLink").css("color","#000");
         $(this).css("background","#446791");
+        $("#bluebar").css("background","#446791");
         $(this).css("color","#FFF");
-        $("body").css("background","#FFF");
+        /*$("body").css("background","#FFF");*/
         $("#topBuffer").animate({height:30},300);
     }
   });
